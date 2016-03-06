529b4675132793ff67bca40be3e3491d4b84c80a
zhouzhou <1197236910@qq.com>
2016-03-06 21:38:25 +0800

    Signed-off-by: zhouzhou <1197236910@qq.com>

--- TestBS4.py
+++ TestBS4.py
@@ -4,5 +4,10 @@
 from urllib.request import urlopen
 from bs4 import  BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-bsObj =BeautifulSoup(html.read())
+if html is None:
+    print("URL is not found")
+else:
+    bsObj =BeautifulSoup(html.read())
+
 print(bsObj.h1)
+print(bsObj.nonExistTag.someTag)
